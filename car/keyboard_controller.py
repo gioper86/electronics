faa1ccb711f7fd101b9025afc4a72cb90529bff3
Giovanni Perna <giovanni.perna@glovoapp.com>
2021-02-05 22:51:57 +0100

    Add a simple keyboard controller

---
+++ car/keyboard_controller.py
@@ -0,0 +1,51 @@
+from pynput import keyboard
+import paho.mqtt.client as mqtt #import the client1
+broker_address="" 
+
+client = mqtt.Client("macGio")
+client.connect(broker_address)
+
+keys = {
+    "w": False,
+    "s": False,
+    "d": False,
+    "a": False
+}
+
+def on_press(key):
+    try:
+        if key.char in keys and not keys[key.char]:
+            keys[key.char] = True
+            publish_message(key.char, "pressed")
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+def on_release(key):
+    print('{0} released'.format(key))
+    if key.char in keys:
+        keys[key.char] = False
+        publish_message(key.char, "released")
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+def publish_message(key, action):
+    actions = {
+        ("w", "pressed"): "forward",
+        ("w", "released"): "stop",
+        ("s", "pressed"): "backward",
+        ("s", "released"): "stop",
+        ("d", "pressed"): "right",
+        ("d", "released"): "straight",
+        ("a", "pressed"): "left",
+        ("a", "released"): "straight"          
+    }
+
+    if (key, action) in actions:
+        client.publish("testGio", actions[(key,action)])
+
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
